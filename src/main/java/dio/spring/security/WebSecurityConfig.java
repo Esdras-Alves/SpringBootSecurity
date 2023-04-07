3dfe68c9ef70cd37c224bdda9da6200c24140dbd
Esdras <esdrasbalves10@gmail.com>
2023-04-07 12:34:27 -0300

    second commit

--- src/main/java/dio/spring/security/WebSecurityConfig.java
+++ src/main/java/dio/spring/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package dio.spring.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +13,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+                .anyRequest().authenticated().and().formLogin();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
--- src/main/java/dio/spring/security/WelcomeController.java
+++ src/main/java/dio/spring/security/WelcomeController.java
@@ -11,12 +11,10 @@ public class WelcomeController {
         return "Welcome to My Spring Boot Web API";
     }
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
     public String users() {
         return "Authorized user";
     }
     @GetMapping("/managers")
-    @PreAuthorize("hasRole('MANAGERS')")
     public String managers() {
         return "Authorized manager";
     }
